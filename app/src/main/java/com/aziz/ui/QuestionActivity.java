cf7ebb98d4988f4873a6f4131b40ac90d13823f6
Kashkula <konoshov947@mail.ru>
2020-11-06 15:00:43 +0600

    second work

---
+++ app/src/main/java/com/aziz/ui/QuestionActivity.java
@@ -0,0 +1,16 @@
+package com.aziz.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aziz.R;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
